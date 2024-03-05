b460aec732fc0dcf18ecec3c7f05fd637a196e8a
William Debotte <wdebotte@student.42.fr>
2024-03-05 13:29:43 +0100

    ex04 done

--- python0_starting/ex04/whatis.py
+++ python0_starting/ex04/whatis.py
@@ -1,13 +1,13 @@
 import sys
 
-# This function print the wheter the number given in parameter is ever or not
+# This function print the number given in parameter is even or not
 def printEvenStatus(number: int):
 	print("I'm Even.") if int(number) % 2 == 0 else print("I'm odd.")
 
 # We first check the len of the argv thanks to sys library, if it's equal to 1 => no arguments given
-# Next, if the condition (len(argv) == 2) is false, then throw AssertionError thankt to assert
-# Next, we'll check if the cast in int of the argv[1] is possible, if not, throw error
-# Then, call isEven function to print the status of the number
+# Next, if the condition (len(argv) == 2) is false, then throw AssertionError thanks to assert it means that we haven't enough arguments
+# Next, we'll check if the cast in int of the argv[1] is possible, if not, throw error, it means that the argument is not a number
+# Then, call printEvenStatus function to print the status of the number
 try:
 	if (len(sys.argv) == 1):
 		exit()
