00326a028cf44cfe50a6ef8a827be55e40537ce9
William Debotte <wdebotte@student.42.fr>
2024-03-14 11:27:55 +0100

    Modification on load_image of ex 02 and finish ex03

---
+++ python1_array/ex03/zoom.py
@@ -0,0 +1,35 @@
+from load_image import ft_load
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def zoom(arrImage: np.array) -> np.array:
+    """Zoom on image by given an numpy array of existed image
+
+    Args:
+        arrImage (np.array): array of image
+
+    Returns:
+        np.array: array of zoomed image
+    """
+    # Slice the array of the image by using Y and X axis
+    arrZoomedImage = arrImage[100:500, 450:850, 0:1]
+    print("New shape after slicing:", np.shape(arrZoomedImage))
+
+    plt.imshow(arrZoomedImage, cmap="grey")
+    plt.show()
+    return arrZoomedImage
+
+
+def main():
+    """Call ft_load function to get array of image and zoom and it,
+    Print both returns arrays
+    """
+    arrImage = ft_load("animal.jpeg")
+    print(arrImage)
+    print(zoom(arrImage))
+
+
+if __name__ == "__main__":
+    main()
