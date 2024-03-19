201632bf87826795cc8c647c56dab57f7999c49f
William Debotte <wdebotte@student.42.fr>
2024-03-19 13:51:04 +0100

    Fix arguments errors for ex05

--- python1_array/ex05/pimp_image.py
+++ python1_array/ex05/pimp_image.py
@@ -11,9 +11,14 @@ def ft_invert(array: np.array) -> np.array:
     Returns:
         np.array: modified image array
     """
-    # This will add 255 - the value of each colors in each dimensions (RGB)
-    invertedArray = np.copy(array)
-    invertedArray[:, :, :3] = 255 - invertedArray[:, :, :3]
+    try:
+        # This will add 255 - the value of each colors in each dimensions (RGB)
+        invertedArray = np.copy(array)
+        invertedArray[:, :, :3] = 255 - invertedArray[:, :, :3]
+    except (IndexError, TypeError) as error:
+        print("Error: Something went wrong with the given array")
+        print("- ", error)
+        exit()
 
     plt.imshow(invertedArray)
     plt.title("Inverted")
@@ -31,12 +36,17 @@ def ft_red(array: np.array) -> np.array:
     Returns:
         np.array: modified image array
     """
-    # This mask will turn to 0 G and B values in all dimensions
-    # Multiply the array by 0 will turn G and B values to 0,
-    # Red is not touched because it's multiplied by 1
-    redMask = np.array([1, 0, 0])
-    redArray = np.copy(array)
-    redArray = redArray * redMask
+    try:
+        # This mask will turn to 0 G and B values in all dimensions
+        # Multiply the array by 0 will turn G and B values to 0,
+        # Red is not touched because it's multiplied by 1
+        redMask = np.array([1, 0, 0])
+        redArray = np.copy(array)
+        redArray = redArray * redMask
+    except (IndexError, TypeError) as error:
+        print("Error: Something went wrong with the given array")
+        print("- ", error)
+        exit()
 
     plt.imshow(redArray)
     plt.title("Red")
@@ -54,11 +64,16 @@ def ft_green(array: np.array) -> np.array:
     Returns:
         np.array: modified image array
     """
-    # Here we'll extract green only from the original array by creating
-    # an empty array filled of 0
-    # and get the green dimension of the original array to overwrite the 0 ones
-    greenArray = np.zeros_like(array)
-    greenArray[:, :, 1] = array[:, :, 1]
+    try:
+        # Here we'll extract green only from the original array by creating
+        # an empty array filled of 0
+        # and get the green dimension of the original array to overwrite the 0 ones
+        greenArray = np.zeros_like(array)
+        greenArray[:, :, 1] = array[:, :, 1]
+    except (IndexError, TypeError) as error:
+        print("Error: Something went wrong with the given array")
+        print("- ", error)
+        exit()
 
     plt.imshow(greenArray)
     plt.title("Green")
@@ -76,11 +91,16 @@ def ft_blue(array: np.array) -> np.array:
     Returns:
         np.array: modified image array
     """
-    # Here we'll extract blue only from the original array by creating
-    # an empty array filled of 0
-    # and get the blue dimension of the original array to overwrite the 0 ones
-    blueArray = np.zeros_like(array)
-    blueArray[:, :, 2] = array[:, :, 2]
+    try:
+        # Here we'll extract blue only from the original array by creating
+        # an empty array filled of 0
+        # and get the blue dimension of the original array to overwrite the 0 ones
+        blueArray = np.zeros_like(array)
+        blueArray[:, :, 2] = array[:, :, 2]
+    except (IndexError, TypeError) as error:
+        print("Error: Something went wrong with the given array")
+        print("- ", error)
+        exit()
 
     plt.imshow(blueArray)
     plt.title("Blue")
@@ -98,16 +118,21 @@ def ft_grey(array: np.array) -> np.array:
     Returns:
         np.array: modified image array
     """
-    redMask = array[:, :, 0] / 3
-    greenMask = array[:, :, 1] / 3
-    blueMask = array[:, :, 2] / 3
-    greyMask = redMask + greenMask + blueMask
+    try:
+        redMask = array[:, :, 0] / 3
+        greenMask = array[:, :, 1] / 3
+        blueMask = array[:, :, 2] / 3
+        greyMask = redMask + greenMask + blueMask
 
-    # Overlay all masks in a 3 dimension and transform floats to int
-    # that image can be translated with plt
-    greyImage = np \
-        .stack([greyMask, greyMask, greyMask], axis=2) \
-        .astype(np.uint8)
+        # Overlay all masks in a 3 dimension and transform floats to int
+        # that image can be translated with plt
+        greyImage = np \
+            .stack([greyMask, greyMask, greyMask], axis=2) \
+            .astype(np.uint8)
+    except (IndexError, TypeError) as error:
+        print("Error: Something went wrong with the given array")
+        print("- ", error)
+        exit()
 
     plt.imshow(greyImage)
     plt.title("Grey")
