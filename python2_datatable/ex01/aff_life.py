5f8af208e929528485a71d5c0da145c28a35e0b0
William Debotte <wdebotte@student.42.fr>
2024-03-21 15:31:43 +0100

    ex01 done

--- python2_datatable/ex01/aff_life.py
+++ python2_datatable/ex01/aff_life.py
@@ -1,16 +1,29 @@
-import pandas as pnd
 import matplotlib.pyplot as plt
-
 from load_csv import load
 
 
 def main():
-    dfFrance = load("../life_expectancy_years.csv")
+    dataFrame = load("../life_expectancy_years.csv")
 
-    print(dfFrance)
-    # df = dfFrance.loc[dfFrance.country == 'France'].T
-    # print(df.axes)
-    # # plt.show()
+    franceData = dataFrame.loc[dataFrame.country == 'France']
+
+    # Retrieve all columns values that is the years except de first one
+    # which refers to 'country'
+    tableYears = franceData.columns[1:]
+    # Retrieve the datas of each columns of the dataset
+    # [0][1:] means taking all elements of the first table except the first one
+    # the first one is 'France'
+    tableDatas = franceData.values[0][1:]
+
+    plt.plot(tableYears, tableDatas)
+    plt.title("France Life expectancy Projections")
+
+    plt.xlabel("Year")
+    # Give copy of years table to xticks function by slicing with step 40
+    plt.xticks(tableYears[::40])
+
+    plt.ylabel("Life expectancy")
+    plt.show()
 
 
 if __name__ == "__main__":
