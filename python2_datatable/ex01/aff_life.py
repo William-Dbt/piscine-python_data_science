23cf7551885c8d77a1abbb0a5642b0cdac4a032c
William Debotte <wdebotte@student.42.fr>
2024-04-10 14:21:21 +0200

    Add comments

--- python2_datatable/ex01/aff_life.py
+++ python2_datatable/ex01/aff_life.py
@@ -5,14 +5,15 @@ from load_csv import load
 def main():
     dataFrame = load("../life_expectancy_years.csv")
 
+	# Get values from dataFrame where country is 'France'
     franceData = dataFrame.loc[dataFrame.country == 'France']
 
     # Retrieve all columns values that is the years except de first one
     # which refers to 'country'
     tableYears = franceData.columns[1:]
     # Retrieve the datas of each columns of the dataset
-    # [0][1:] means taking all elements of the first table except the first one
-    # the first one is 'France'
+    # .values[0][1:] means taking all elements of the first table except
+    # the first one which is 'France'
     tableDatas = franceData.values[0][1:]
 
     plt.plot(tableYears, tableDatas)
